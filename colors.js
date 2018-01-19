382a90f4528d944b000c3aa4b06f55f37472ab7e
myriampace <35174729+myriampace@users.noreply.github.com>
2018-01-19 16:35:05 +0100

    Update colors.js

--- colors.js
+++ colors.js
@@ -6,13 +6,12 @@ var colors = ['black', 'grey', 'white', 'red', 'orange', 'yellow', 'green', 'blu
   swatches[i].addEventListener('click', setSwatch);
 }*/
 
-// crée les ronds de couleurs plus vite que de tout taper à la main
 for(var i = 0, n = colors.length; i < n; i++) {
-  var swatch = document.createElement('div');   // crée des éléments div dans l'index
-  swatch.className = 'swatch';    // donne la classe swatch à ces éléments div
-  swatch.style.backgroundColor = colors[i];   // donne la couleur de fond aux ronds
-  swatch.addEventListener('click', setSwatch);    // associe un événement(fonction) click qui s'appelle setSwatch
-  document.getElementById('colors').appendChild(swatch);    // ajoute dans le div colors les swatch qu'on a créée
+  var swatch = document.createElement('div');  
+  swatch.className = 'swatch';   
+  swatch.style.backgroundColor = colors[i];   
+  swatch.addEventListener('click', setSwatch);   
+  document.getElementById('colors').appendChild(swatch);   
 }
 
 function setColor(color) {
@@ -26,7 +25,7 @@ function setColor(color) {
 function setSwatch(e) {
   // identify swatch
   var swatch = e.target;
-  // set color function sets to the background color of the swatch
+  // sets to the background color of the swatch
   setColor(swatch.style.backgroundColor);
   // give active class
   swatch.className += ' active';
