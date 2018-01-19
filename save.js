a51928a4e80e857363eb3bccf4db4512b65344b3
myriampace <35174729+myriampace@users.noreply.github.com>
2018-01-19 16:37:56 +0100

    Update save.js

--- save.js
+++ save.js
@@ -3,7 +3,6 @@ var saveButton = document.getElementById('save');
 
 saveButton.addEventListener('click', saveImage);
 
-// fonction save image est liée au button save et s'active à l'évenemnt click
 function saveImage() {
   var data = canvas.toDataURL();
 
@@ -11,9 +10,8 @@ function saveImage() {
 
   request.onreadystatechange = function() {
     if(request.readyState == 4 && request.status == 200) {
-      // do our stuff
       var response = request.responseText;
-      window.open('download.php?file='+response, '_blank', 'location=0, menubar=o');  // fonction that opens a new window
+      window.open('download.php?file='+response, '_blank', 'location=0, menubar=o');
     }
   }
 
@@ -21,5 +19,5 @@ function saveImage() {
   request.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
   request.send('img='+data);
 
-  //window.open(response, '_blank', 'location=0, menubar=o');  // fonction that opens a new window
+  //window.open(response, '_blank', 'location=0, menubar=o'); 
 }
